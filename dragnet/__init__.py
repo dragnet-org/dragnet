4ff256662ab42b90ea268c8603e542b6ade95307
Matthew Peters <matt@seomoz.org>
2012-08-31 07:58:09 -0700

    __init__ and setup.py

--- dragnet/__init__.py
+++ dragnet/__init__.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
 
 from .arias import Arias
-from .kohlschuetter import Kohlschuetter
+from .kohlschuetter import KohlschuetterBase, Kohlschuetter, KohlschuetterNormalized, DragnetModel, DragnetModelKohlschuetterFeatures
+from .logistic_regression import LogisticRegression
