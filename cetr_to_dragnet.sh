c67e74364c98655d875ccc360ac094c0af74fb01
Matthew Peters <matt@seomoz.org>
2013-02-12 12:24:22 -0800

    Convert CETR news articles data to Dragnet

--- cetr_to_dragnet.sh
+++ cetr_to_dragnet.sh
@@ -2,80 +2,121 @@
 
 set -e
 
-DATADIR=/Users/matt/data/dragnet/cetr/tt/cleaneval/zh/
+if [ -z "$1" ]
+then
+    echo "Convert CETR to dragnet format"
+    echo ""
+    echo "Usage: $0 rootdir"
+    exit 1
+fi
 
-pushd $DATADIR
+ROOTDIR=$1
+DATADIR=$1/cleaneval/en/
 
-# rename HTML, copy over all .html files, delete the rest
-mkdir HTML
-mv original/*.html HTML
-rm -r original
+for D in en zh bbc freep myriad nypost nytimes reuters suntimes techweb tribune
+do
 
-# rename/move the corrected files
-mkdir Corrected
-ls gold/*.txt | perl -pe 'm/\/([0-9]+)\.txt/; system "cp gold/$1.txt Corrected/$1.html.corrected.txt\n";'
+    if [[ "$D" == "en" || "$D" == "zh" ]]
+    then
+        CLEANEVAL=1
+        DATADIR=$1/cleaneval/$D/
+    else
+        CLEANEVAL=0
+        DATADIR=$1/news/$D/
+    fi
 
-# block_corrected
-mkdir block_corrected
+    echo "Processing $D"
+    
+    # for news, CETR has a slightly different directory structure
+    if [[ $CLEANEVAL == 0 ]]
+    then
+        mkdir $DATADIR
+        mkdir $DATADIR/gold
+        mkdir $DATADIR/original
+        mv $1/news/gold/$D/*.txt $DATADIR/gold
+        mv $1/news/original/$D/*.html $DATADIR/original
+    fi
 
-rm -r gold
-
-# remove all files in HTML that are not in Corrected
-# and vice versa
-perl -e '
-@html_files = glob "HTML/*";
-@corrected_files = glob "Corrected/*";
-%html = ();
-foreach (@html_files) {
-    $m/\/([0-9]+)\.html/;
-    $html{$1} = 1;
-}
-
-%corrected = ();
-foreach (@corrected_files) {
-    $m/\/([0-9]+)\.html/;
-    $corrected{$1} = 1;
-}
-
-# now check html.  if not in corrected then delete
-while ( ($key, $value) = each(%html) ) {
-    if (!defined $corrected{$key}) {
-        system "rm HTML/$key.html";
+    pushd $DATADIR
+    
+    # rename HTML, copy over all .html files, delete the rest
+    mkdir HTML
+    mv original/*.html HTML
+    rm -r original
+    
+    # rename/move the corrected files
+    mkdir Corrected
+    ls gold/*.txt | perl -pe 'm/\/([0-9]+)\.txt/; system "cp gold/$1.txt Corrected/$1.html.corrected.txt\n";'
+    rm -r gold
+    
+    # block_corrected
+    mkdir block_corrected
+    
+    # remove all files in HTML that are not in Corrected
+    # and vice versa
+    perl -e '
+    @html_files = glob "HTML/*";
+    @corrected_files = glob "Corrected/*";
+    %html = ();
+    foreach (@html_files) {
+        $m/\/([0-9]+)\.html/;
+        $html{$1} = 1;
     }
-}
-
-# vice versa
-while ( ($key, $value) = each(%corrected) ) {
-    if (!defined $html{$key}) {
-        system "rm Corrected/$key.html.corrected.txt";
+    
+    %corrected = ();
+    foreach (@corrected_files) {
+        $m/\/([0-9]+)\.html/;
+        $corrected{$1} = 1;
     }
-}
-'
-
-
-# now need to massage the formatting of the files
-# need to remove the first line
-ls HTML/* Corrected/* | perl -pe '
-$orig_file = $_;
-open (F, $orig_file);
-open (FOUT, ">t");
-$k=0;
-while (<F>) {
-    if ($k > 0) {
-        print FOUT $_;
+    
+    # now check html.  if not in corrected then delete
+    while ( ($key, $value) = each(%html) ) {
+        if (!defined $corrected{$key}) {
+            system "rm HTML/$key.html";
+        }
     }
-    $k++;
-}
-close(FOUT);
-close(F);
-system "mv t $orig_file";
-'
+    
+    # vice versa
+    while ( ($key, $value) = each(%corrected) ) {
+        if (!defined $html{$key}) {
+            system "rm Corrected/$key.html.corrected.txt";
+        }
+    }
+    '
+    
+    
+    # now need to massage the formatting of the files
+    # need to remove the first line of Corrected file
+    if [[ $CLEANEVAL == 1 ]]
+    then
+        echo "Cleaning corrected files"
+        ls Corrected/* | perl -pe '
+        $orig_file = $_;
+        open (F, $orig_file);
+        open (FOUT, ">t");
+        $k=0;
+        while (<F>) {
+            if ($k > 0) {
+                print FOUT $_;
+            }
+            $k++;
+        }
+        close(FOUT);
+        close(F);
+        system "mv t $orig_file";
+        '
+    fi
+    
+    
+    # clean up some things
+    rm -r vips || true
+    rm -r stripped || true
+    
+    popd
 
-rm -r stripped
+done
 
-# vips only exists for some of the data files
-rm -r vips || true
-
-popd
-
+rm -r $1/news/gold
+rm -r $1/news/original
+rm -r $1/news/vips
 
