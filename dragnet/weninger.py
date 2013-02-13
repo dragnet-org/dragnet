61ae27b2d995a09b2ba55df8102919b73419844b
Matthew Peters <matt@seomoz.org>
2013-02-13 09:27:10 -0800

    Initial commit of Weninger implementation

---
+++ dragnet/weninger.py
@@ -0,0 +1,57 @@
+
+# inspired by
+#Weninger, Tim, William H. Hsu, and Jiawei Han. "CETR: content extraction via
+#    tag ratios." Proceedings of the 19th international conference on
+#    World wide web. ACM, 2010.
+
+import numpy as np
+from scipy.ndimage import gaussian_filter
+from .content_extraction_model import ContentExtractionModel, IdentityPredictor
+from .blocks import Blockifier
+from .kmeans import KMeansFixedOrigin
+
+def weninger_sx_sdx(x):
+    """blocks = a 1D array of something (their content-tag-ratios, length, ..)
+    Computes and returns the smoothed version of x and
+    absolute smoothed discount (eqn 4)"""
+    sigma = 1.0
+    alpha = 3
+
+    nx = len(x)
+
+    # compute absolute discount
+    dx = np.zeros(nx)
+    for k in xrange(nx-1):
+        dx[k] = abs(np.mean(x[(k + 1):min(k + 1 + alpha, nx)]) - x[k])
+    dx[nx-1] = abs(np.mean(x[-2:]) - x[nx-1])
+
+    # smooth
+    sx = gaussian_filter(x, sigma=sigma)
+    sdx = gaussian_filter(dx, sigma=sigma)
+
+    return np.hstack((sx.reshape(-1, 1), sdx.reshape(-1, 1)))
+weninger_sx_sdx.nfeatures = 2
+
+
+class WeningerFeatures(object):
+    nfeatures = 1
+
+    def __init__(self, clusters=3):
+        self._clusters = clusters
+
+    def __call__(self, blocks, train=False):
+        sx_sdx = weninger_sx_sdx([len(block.text) for block in blocks])
+
+        km = KMeansFixedOrigin(self._clusters)
+        km.fit(sx_sdx)
+        content = km.closest_centers(sx_sdx) > 0
+        return content.astype(np.int).reshape(-1, 1)
+
+
+class Weninger(ContentExtractionModel):
+    def __init__(self, clusters=3, blockifier=Blockifier, **kwargs):
+        features = [WeningerFeatures(clusters)]
+        ContentExtractionModel.__init__(self, blockifier, features, IdentityPredictor, **kwargs)
+
+
+
