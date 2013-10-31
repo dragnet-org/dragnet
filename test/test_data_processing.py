0a612a49c2a98f5e6ab009769b24aaf02da98d5e
Matthew Peters <matt@moz.com>
2013-10-31 10:43:30 -0700

    Added Makefile and made fixtures directory consistent in all tests

--- test/test_data_processing.py
+++ test/test_data_processing.py
@@ -8,6 +8,8 @@ import os
 
 from shutil import rmtree
 
+FIXTURES = 'test/datafiles'
+
 class Testget_list_all_corrected_files(unittest.TestCase):
     def test_list_all_files(self):
         prefixes = ['bbc.co.story', 'f1', 'sad8-2sdkfj']
@@ -32,19 +34,21 @@ class Testread_gold_standard(unittest.TestCase):
     actual_chinese_content = u'\n\n            <h>\u9ad8\u8003\u8bed\u6587\u5168\u7a0b\u68c0\u6d4b\u4e09\uff1a\u6b63\u786e\u4f7f\u7528\u8bcd\u8bed\uff08\u719f\u8bed\u4e00\uff09\n\n\n            <h>LEARNING.SOHU.COM    2004\u5e745\u670822\u65e515:36   \n\n\n   '
 
     def test_read_gold_standard(self):
-        all_files = data_processing.get_list_all_corrected_files("datafiles")
+        all_files = data_processing.get_list_all_corrected_files(FIXTURES)
         all_files.sort()
 
         chars = {'ascii':u'ascii yo!', 'iso-8859-1':u'\xd3', 'utf-8':u'\xae', 'utf-16':u'\xae'}
 
         for e in chars:
-            content_comments = data_processing.read_gold_standard("datafiles", e)
+            content_comments = data_processing.read_gold_standard(
+                FIXTURES, e)
             actual_content = (u"Content here\nmore content\n" + chars[e] + u"\n")
             self.assertEqual(content_comments[0], actual_content)
             self.assertEqual(content_comments[1], '\nsome comments\n')
 
     def test_utf8(self):
-        gs = ' '.join(data_processing.read_gold_standard("datafiles", 'utf-8_chinese'))
+        gs = ' '.join(data_processing.read_gold_standard(FIXTURES,
+            'utf-8_chinese'))
         self.assertEqual(gs, Testread_gold_standard.actual_chinese_content)
 
 
@@ -52,7 +56,7 @@ class Testread_gold_standard(unittest.TestCase):
 
 class Testextract_gold_standard(unittest.TestCase):
 
-    datadir = "datafiles"
+    datadir = FIXTURES
 
     @staticmethod
     def _remove_output(names, catch_errors, name_maker=None):
--- test/test_models.py
+++ test/test_models.py
@@ -1,12 +1,16 @@
 
 import unittest
 import json
+import os.path
 from dragnet.models import * 
+
+FIXTURES = 'test/datafiles'
 
 class TestModels(unittest.TestCase):
     def test_models(self):
 
-        html_string = open('datafiles/models_testing.html', 'r').read()
+        html_string = open(os.path.join(
+            FIXTURES, 'models_testing.html'), 'r').read()
 
         models = [kohlschuetter_model,
                   weninger_model, 
@@ -16,7 +20,8 @@ class TestModels(unittest.TestCase):
 
         content = [m.analyze(html_string) for m in models]
 
-        actual_content = json.load(open('datafiles/models_content.json', 'r'))
+        actual_content = json.load(open(
+            os.path.join(FIXTURES, 'models_content.json'), 'r'))
 
         for k in xrange(len(content)):
             self.assertTrue(actual_content[k] == content[k])
