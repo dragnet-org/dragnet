c4a5632a1ca7543d3263c0792fecccfed6f3588b
Matthew Peters <matt@moz.com>
2013-10-31 10:38:24 -0700

    Remove commented out code

--- setup.py
+++ setup.py
@@ -21,10 +21,6 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#try:
-#    from setuptools import setup
-#except ImportError:
-
 from distutils.core import setup
 from numpy import get_include
 from distutils.extension import Extension
