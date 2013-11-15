b82a64c0fad8a0c28e0f9bfb83d0092594fa4f23
Matthew Peters <matt@moz.com>
2013-11-14 17:35:41 -0800

    Made test_models.py more robust

--- test/test_models.py
+++ test/test_models.py
@@ -18,13 +18,23 @@ class TestModels(unittest.TestCase):
                   kohlschuetter_css_model,
                   kohlschuetter_css_weninger_model]
 
-        content = [m.analyze(html) for m in models]
-
         actual_content = json.load(open(
             os.path.join(FIXTURES, 'models_content.json'), 'r'))
 
-        for k in xrange(len(content)):
-            self.assertTrue(actual_content[k].encode('utf-8') == content[k])
+        for k in xrange(len(models)):
+            # some of the models (weninger) aren't deterministic
+            # so the content doesn't match exactly every time,
+            # although it passes most of the time
+            # we allow a max of 5 failures before failing the entire test
+            m = models[k]
+            passed = False
+            for i in xrange(5):
+                content = m.analyze(html)
+                if actual_content[k].encode('utf-8') == content:
+                    passed = True
+                    break
+            self.assertTrue(passed)
+
 
 if __name__ == "__main__":
     unittest.main()
