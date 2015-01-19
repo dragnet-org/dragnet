45814b73d1955470070474472c8e3241e4bee4f8
Matthew Peters <matt@moz.com>
2015-01-18 21:56:23 -0800

    Remove dead code

--- dragnet/__init__.py
+++ dragnet/__init__.py
@@ -33,23 +33,3 @@ class AllFeatures(object):
         else:
             raise KeyError
 
-
-# we want to maintain backward compatibility with 
-# code that uses the old interface to LogisticRegression
-# and DragnetModel until existing
-# code can be updated to use the new interface
-from mozsci.models import LogisticRegression as lr
-class LogisticRegression(lr):
-    def pred(self, *args, **kwargs):
-        return self.predict(*args, **kwargs)
-
-
-class DragnetModelKohlschuetterFeatures(ContentExtractionModel):
-    def __init__(self, block_model, mean_std, threshold=0.5):
-        """block_model = LogisticRegression
-        mean_std = {'mean':[mean0, mean1, ..], 'std':[std0, std1, ..]}
-        """
-        koh_features = NormalizedFeature(kohlschuetter_features, mean_std)
-        ContentExtractionModel.__init__(self, Blockifier,
-                         [koh_features], block_model, threshold=threshold)
-
