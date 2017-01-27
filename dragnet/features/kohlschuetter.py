6e47adb0a521a7cc68020862364ca00081410dc7
Burton DeWilde <burton@chartbeat.com>
2017-01-27 13:48:22 -0500

    Finally cleared out parallel code from refactor

--- dragnet/features/kohlschuetter.py
+++ dragnet/features/kohlschuetter.py
@@ -18,10 +18,17 @@ class KohlschuetterFeatures(BaseEstimator, TransformerMixin):
     __name__ = 'kohlschuetter'
 
     def fit(self, blocks, y=None):
+        """
+        This method returns the current instance unchanged, since no fitting is
+        required for this ``Feature``. It's here only for API consistency.
+        """
         return self
 
     def transform(self, blocks, y=None):
         """
+        Transform an ordered sequence of blocks into a 2D features matrix with
+        shape (num blocks, num features).
+
         Args:
             blocks (List[Block]): as output by :class:`Blockifier.blockify`
             y (None): This isn't used, it's only here for API consistency.
