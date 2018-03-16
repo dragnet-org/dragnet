52476b9633746ed498f57df50685e65f36a88759
Patrick Kelley <pkelley@bombora.com>
2018-03-15 19:33:30 -0700

    Maintain document-partitioned feature extraction
    Instead of concatenating all blocks before training/predicting, calculate
    features on each HTML document individually.

--- dragnet/model_training.py
+++ dragnet/model_training.py
@@ -4,6 +4,7 @@ import io
 import logging
 import os
 import pprint
+import numpy as np
 
 from sklearn.externals import joblib
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
@@ -29,6 +30,13 @@ def evaluate_model_predictions(y_true, y_pred, weights=None):
     Returns:
         Dict[str, float]
     """
+    if isinstance(y_pred[0], np.ndarray):
+        y_pred = np.concatenate(y_pred)
+    if isinstance(y_true[0], np.ndarray):
+        y_true = np.concatenate(y_true)
+    if (weights is not None) and (isinstance(weights[0], np.ndarray)):
+        weights = np.concatenate(weights)
+
     accuracy = accuracy_score(
         y_true, y_pred, normalize=True, sample_weight=weights)
     precision = precision_score(
@@ -85,25 +93,27 @@ def train_model(extractor, data_dir, output_dir=None):
     # set up directories and file naming
     output_dir, fname_prefix = _set_up_output_dir_and_fname_prefix(output_dir, extractor)
 
-    # prepare, split, and concatenate the data
+    # prepare and split the data
     logging.info('preparing, splitting, and concatenating the data...')
     data = prepare_all_data(data_dir)
     training_data, test_data = train_test_split(
         data, test_size=0.2, random_state=42)
-    train_blocks, train_labels, train_weights = extractor.concatenate_data(training_data)
-    test_blocks, test_labels, test_weights = extractor.concatenate_data(test_data)
+    train_html, train_labels, train_weights = extractor.get_html_labels_weights(training_data)
+    test_html, test_labels, test_weights = extractor.get_html_labels_weights(test_data)
 
     # fit the extractor on training data
     # then evaluate it on train and test data
     logging.info('fitting and evaluating the extractor features and model...')
     try:
-        extractor.fit(train_blocks, train_labels, weights=train_weights)
+        extractor.fit(train_html, train_labels, weights=train_weights)
     except (TypeError, ValueError):
-        extractor.fit(train_blocks, train_labels)
+        extractor.fit(train_html, train_labels)
     train_eval = evaluate_model_predictions(
-        train_labels, extractor.predict(train_blocks))
+        np.concatenate(train_labels), extractor.predict(train_html),
+        np.concatenate(train_weights))
     test_eval = evaluate_model_predictions(
-        test_labels, extractor.predict(test_blocks))
+        np.concatenate(test_labels), extractor.predict(test_html),
+        np.concatenate(test_weights))
 
     # report model performance
     _report_model_performance(output_dir, fname_prefix, train_eval, test_eval)
@@ -149,36 +159,50 @@ def train_many_models(extractor, param_grid, data_dir, output_dir=None,
     # set up directories and file naming
     output_dir, fname_prefix = _set_up_output_dir_and_fname_prefix(output_dir, extractor)
 
-    # prepare, split, and concatenate the data
-    logging.info('preparing, splitting, and concatenating the data...')
+    # prepare and split the data
+    logging.info('preparing and splitting the data...')
     data = prepare_all_data(data_dir)
     training_data, test_data = train_test_split(
         data, test_size=0.2, random_state=42)
-    train_blocks, train_labels, train_weights = extractor.concatenate_data(training_data)
-    test_blocks, test_labels, test_weights = extractor.concatenate_data(test_data)
+    train_html, train_labels, train_weights = extractor.get_html_labels_weights(training_data)
+    test_html, test_labels, test_weights = extractor.get_html_labels_weights(test_data)
+
+    # filter docs we can't get features from
+    train_blocks = np.array([extractor.blockifier.blockify(doc)
+                            for doc in train_html])
+    train_mask = [extractor._has_enough_blocks(blocks) for blocks in train_blocks]
+    train_blocks = train_blocks[train_mask]
+    train_labels = np.concatenate(train_labels[train_mask])
+    train_weights = np.concatenate(train_weights[train_mask])
+    test_labels = np.concatenate(test_labels)
+    test_weights = np.concatenate(test_weights)
+    # get features
+    # TODO: This only 'fit's one doc at a time. No feature fitting actually
+    # happens for now, but this might be important if the features change
+    train_features = np.concatenate([extractor.features.fit_transform(blocks)
+                                    for blocks in train_blocks])
 
     # fit many models
     gscv = GridSearchCV(
-        extractor, param_grid, fit_params={'weights': train_weights},
+        extractor.model, param_grid, fit_params={'sample_weight': train_weights},
         scoring=kwargs.get('scoring', 'f1'), cv=kwargs.get('cv', 5),
         n_jobs=kwargs.get('n_jobs', 1), verbose=kwargs.get('verbose', 1))
-    gscv = gscv.fit(train_blocks, train_labels)
+    gscv = gscv.fit(train_features, train_labels)
 
     logging.info('Score of the best model, on left-out data: %s', gscv.best_score_)
     logging.info('Params of the best model: %s', gscv.best_params_)
 
     # evaluate best model on train and test data
-    best_extractor = gscv.best_estimator_
+    extractor.model = gscv.best_estimator_
     train_eval = evaluate_model_predictions(
-        train_labels, best_extractor.predict(train_blocks))
+        train_labels, extractor.predict(train_html[train_mask]), weights=train_weights)
     test_eval = evaluate_model_predictions(
-        test_labels, best_extractor.predict(test_blocks))
-    _report_model_performance(output_dir, fname_prefix, train_eval, test_eval)
+        test_labels, extractor.predict(test_html), weights=test_weights)
 
     # pickle the final model
-    _write_model_to_disk(output_dir, fname_prefix, best_extractor)
+    _write_model_to_disk(output_dir, fname_prefix, extractor)
 
-    return best_extractor
+    return extractor
 
 
 def _set_up_output_dir_and_fname_prefix(output_dir, extractor):
